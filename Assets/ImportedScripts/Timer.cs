c864e9f70bf393eacaef8f457909efb10a115052
Sankalpa Sarkar <47474648+Sankalpa-Sarkar@users.noreply.github.com>
2020-07-19 13:51:40 +0530

    Added game files to repository
    The relevant game files have been added to the repository, including the scripts, assets and mechanics.

---
+++ Assets/ImportedScripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text timerLabel;
+    public float startTimer = 0;
+
+    private float time;
+
+    void Update()
+    {
+
+
+        if (startTimer > 0)
+        {
+            time += Time.deltaTime;
+
+            var minutes = (int) Mathf.Floor(time / 60); //Divide the guiTime by sixty to get the minutes.
+            var seconds = time % 60;//Use the euclidean division for the seconds.
+            var fraction = (time * 100) % 100;
+
+
+            timerLabel.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
+        }
+        else
+        {
+            //do nothing
+        }
+    }
+}
+
+
---
+++ Assets/ImportedScripts/controlTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlTimer : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name == "startRace")
+        {
+            setTimer(1);
+        }
+
+        if (other.gameObject.name == "finishRace")
+        {
+            setTimer(0);
+        }
+
+    }
+
+    void setTimer(int t)
+    {
+        Timer playerTimer = this.GetComponent<Timer>();
+        playerTimer.startTimer = t;
+    }
+}
